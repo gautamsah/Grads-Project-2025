3d6b0458b5cc026cb850252ea55a750236b57dbe
Jay Patel <jay326patel@gmail.com>
2025-04-02 21:53:39 +0530

    feature/GT25-35 : Updated JS, CSS and Apex

--- force-app/main/default/lwc/resRatingHotelCard/resRatingHotelCard.js
+++ force-app/main/default/lwc/resRatingHotelCard/resRatingHotelCard.js
@@ -1,29 +1,16 @@
-import { LightningElement } from 'lwc';
-
+import { LightningElement, wire } from 'lwc';
 import hoteldetail from '@salesforce/apex/RES_Utility.getRatingHotelCards';
 
 export default class ResHotelCard extends LightningElement{
     getHotels;
-    isDataLoaded = false;
-
     hasRenderd = false;
-    renderedCallback(){
-        console.log("Rendering Feature ");
-        if(this.isDataLoaded) return;
+    connectedCallback(){
         hoteldetail().then(result => {
             this.getHotels = result;
-            
             this.hasRenderd = true;
-            console.log("Data from back", result);
-
-            
         })
         .catch(error => {
             console.error('Error fetching test4 names:', error);
         });
-
-        this.isDataLoaded = true;
-
     }
-     
 }
