9de025529e1508adbbe68a13c37bc7be992ca19f
milinkapatel <milinkapatel007@gmail.com>
2025-07-20 22:30:15 +0530

    only objects with fields

--- force-app/main/default/lwc/resGenericBanner/resGenericBanner.js
+++
@@ -1,9 +0,0 @@
-import { api, LightningElement } from 'lwc';
-
-export default class ResGenericBanner extends LightningElement {
-    @api
-    title;
-
-    @api
-    subtitle;
-}
--- force-app/main/default/lwc/resTermsAndConditions/resTermsAndConditions.js
+++
@@ -1,3 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class ResTermsAndConditions extends LightningElement {}
